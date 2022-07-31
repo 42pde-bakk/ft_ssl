4e9bfcf7f44bb667bfea0edc531b4b5f8e2a48c8
pde-bakk <pde-bakk@student.codam.nl>
2022-07-31 17:06:06 +0200

    messed up the order

--- srcs/utils/copy_four_chars_to_uint32.c
+++ srcs/utils/copy_four_chars_to_uint32.c
@@ -6,9 +6,14 @@
 uint32_t	four_chars_to_uint32(const uint8_t *ptr) {
 	uint32_t ret ;
 
-	ret = ptr[3] << 24 |
-		  ptr[2] << 16 |
-		  ptr[1] << 8 |
-		  ptr[0];
+	ret = ptr[0] << 24 |
+		  ptr[1] << 16 |
+		  ptr[2] << 8 |
+		  ptr[3];
+
+//	ret = ptr[3] << 24 |
+//		  ptr[2] << 16 |
+//		  ptr[1] << 8 |
+//		  ptr[0];
 	return (ret);
 }
